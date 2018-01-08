170d86ff2562c74821050efe9f859862a8ab2fa7
anat ana <anat19960@gmail.com>
2018-01-08 18:06:16 +0200

    BlockChain and Block classes implementation

---
+++ src/main/java/blockChain/Block.java
@@ -0,0 +1,60 @@
+package blockChain;
+
+import transactions.AbstractTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anat ana on 07/01/2018.
+ */
+
+
+public class Block {
+
+    private List<AbstractTransaction> transactions;
+    private long timestamp;
+    private String blockId;
+    /*optional:
+    * Set<AbstractTransaction> pool;
+    * and information about previous transactions
+    * */
+
+    public Block(String blockId) {
+        this.transactions = new ArrayList<>();
+        this.timestamp = System.currentTimeMillis();
+        this.blockId = blockId;
+    }
+
+    public Block(List<AbstractTransaction> transactions, String blockId) {
+        this.transactions = transactions;
+        timestamp = System.currentTimeMillis();
+        this.blockId = blockId;
+    }
+
+    public long getTimestamp() {
+        return this.timestamp;
+    }
+
+    public List<AbstractTransaction> getTransactions() {
+        return this.transactions;
+    }
+
+    public String getBlockId() {
+        return this.blockId;
+    }
+
+    public AbstractTransaction getTransactionById(Block block, String id) {
+        for (AbstractTransaction transaction : block.getTransactions()) {
+            if (transaction.getItemID().equals(id)) {
+                return transaction;
+            }
+        }
+        return null;
+    }
+
+    public void addTransactionToBlock(AbstractTransaction transaction) {
+        this.getTransactions().add(transaction);
+    }
+
+}
